41665f8e76a6bd372a6a3f609c5964d403950cdc
halyna20 <halynka2000@gmail.com>
2022-04-28 21:03:50 +0300

    add end function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 startBtn = document.querySelector('#start-btn');
 startScreen = document.querySelector('.start');
 mainScreen = document.querySelector('.main');
-player = null;
+endScreen = document.querySelector('.finish');
 numScores = 0;
 
 function getRand(min, max) {
@@ -16,11 +16,11 @@ startBtn.onclick = function () {
     createHealth();
     createPlayer();
 
-    //setTimeout(function () {
-    //    ast.style.top = getRand(550, 600) + "px";
-    //    ast.style.left = getRand(300, 330) + "px";
-    //    collision();
-    //}, 200);
+    setTimeout(function () {
+        ast.style.top = getRand(550, 600) + "px";
+        ast.style.left = getRand(300, 330) + "px";
+        collision();
+    }, 200);
 }
 
 
@@ -56,19 +56,26 @@ function createPlayer() {
     mainScreen.appendChild(player);
 }
 
-//function collision() {
-//    let positionAst = ast.offsetLeft - ast.offsetWidth;
-//    let player = document.querySelector('.player');
-//    let positionPlayer = player.offsetLeft - player.offsetWidth;
+function collision() {
+    let positionAst = ast.offsetLeft - ast.offsetWidth;
+    let player = document.querySelector('.player');
+    let positionPlayer = player.offsetLeft - player.offsetWidth;
 
-//    if (ast.offsetLeft + ast.offsetWidth >= player.offsetLeft && ast.offsetLeft <= player.offsetLeft + player.offsetWidth) {
-//        if (ast.offsetTop >= player.offsetTop - player.offsetHeight && ast.offsetTop <= player.offsetTop) {
-//            health = document.querySelector('.health-bar');
-//            health.style.width = health.offsetWidth - 20 + 'px';
-//        }
-//    }
-//}
+    if (ast.offsetLeft + ast.offsetWidth >= player.offsetLeft && ast.offsetLeft <= player.offsetLeft + player.offsetWidth) {
+        if (ast.offsetTop >= player.offsetTop - player.offsetHeight && ast.offsetTop <= player.offsetTop) {
+            health = document.querySelector('.health-bar');
+            health.style.width = health.offsetWidth - 20 + 'px';
+            gameEnd(health);
+        }
+    }
+}
 
+function gameEnd(health) {
+    param = parseInt(health.style.width);
+    if (param <= 0) {
+        mainScreen.style.display = 'none';
+    }
+}
 
 document.onkeydown = function (e) {
     if (e.keyCode == 39) {
