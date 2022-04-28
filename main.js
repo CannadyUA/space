0bae871df514a32c45922072eafafa332c6ad4ef
Alex Soloviov <busdriver1593@gmail.com>
2022-04-28 14:58:28 +0300

    add audio fix control

--- main.js
+++ main.js
@@ -4,9 +4,21 @@ mainScreen = document.querySelector('.main');
 player = null;
 numLives = 3;
 numScores = 0;
+asteroid = null;
+ufo = null;
+
+shotAudio = document.createElement('audio');
+shotAudio.src = 'audio/shot.mp3';
+
+killEnemy = document.createElement('audio');
+killEnemy.src = 'audio/killenemy.mp3';
+
+
+maintheme = document.createElement('audio');
+maintheme.src = 'audio/maintheme.mp3';
 
 function getRand(min, max) {
-  return Math.random() * (max - min) + min;
+    return (Math.floor(Math.random() * (max - min)) + min);
 }
 
 startBtn.onclick = function () {
@@ -14,6 +26,7 @@ startBtn.onclick = function () {
     createScore();
     createLives();
     createPlayer();
+    maintheme.play();
 }
 
 function createScore() {    
@@ -28,10 +41,6 @@ function createLives() {
     lives.className = 'lives';
     lives.innerText = 'x' + numLives;
     mainScreen.appendChild(lives);
-}
-
-function createAsteroid() {
-    
 }
 
 function createUFO() {
@@ -49,13 +58,16 @@ document.onkeydown = function (e) {
     
     if (e.keyCode == 39) {
         player.style.left = player.offsetLeft + 70 + 'px';
-       if (player.offsetLeft > 500) {
+       if (player.offsetLeft > 450) {
            player.style.left = 505 + 'px';
        }
     } else if (e.keyCode == 37) {
         player.style.left = player.offsetLeft - 70 + 'px';
-        if (player.offsetLeft < 10) {
+        if (player.offsetLeft < 50) {
            player.style.left = 5 + 'px'; 
         }
     }
 }
+
+
+
