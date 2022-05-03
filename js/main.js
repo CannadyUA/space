2f07d998326c3adf2129552a0099cd45d6c8d099
Roman Melnychuk <roman4iek02@gmail.com>
2022-05-03 19:07:57 +0300

    pre-release

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ endScreen = document.querySelector('.finish');
 winScreen = document.querySelector('.win');
 player = document.querySelector('.player');
 soundBtn = document.querySelector(".sound");
+comment = document.querySelector('.comment');
+startComment = document.querySelector('.start-comment');
 moveBoss = 0;
 bossShoting = 0;
 timerID = 0;
@@ -36,6 +38,11 @@ gameOver.src = 'audio/game-over.mp3';
 killUfo = document.createElement('audio');
 killUfo.src = 'audio/kill.mp3';
 
+letsGetHim = document.createElement('audio');
+letsGetHim.src = 'audio/lets-get-him.mp3';
+
+houston = document.createElement('audio');
+houston.src = 'audio/houston.mp3';
 
 //helping function
 function getRand(min, max) {
@@ -55,6 +62,16 @@ function startGame() {
     mainScreen.style.display = "block";
     mainAudio.play();
     mainAudio.loop = true;
+
+    startComment.innerText = 'The forces of darkness have invaded our Galaxy. And only the power of Purity and Valor can restore Justice';
+    startComment.classList.add('start-comment-anim');
+    setTimeout(function(){
+        startComment.classList.remove('start-comment-anim');
+        startComment.classList.add('start-comment-anim-fade');
+        setTimeout(function() {
+            startComment.remove();
+        },4000);
+    }, 5000);
     delay = getRand(1500, 2500); //затримка для астероїдів
     longDelay = getRand(3500, 5000);
     setInterval(createAsteroid, 3600);
@@ -70,8 +87,19 @@ function createScore() {
     score.innerText = 'SCORE: ' + numScores;
     mainScreen.appendChild(score);
     gameBlock = document.querySelector('._block');
-    if (numScores === 200 && isBoss === false || numScores == 210 && isBoss === false) {
+    if (numScores === 10 && isBoss === false || numScores == 210 && isBoss === false) {
         bossLevel();
+        houston.play();
+        comment.innerHTML = 'Ooh..<br>Houston, we have a problem';
+        setTimeout(function() {
+            comment.classList.add('comment-anim');
+            comment.innerText = "Let's get him!!!";
+            letsGetHim.play();
+            setTimeout(function() {
+                comment.remove();
+            },3000);
+        }, 4000);
+
     }
 }
 
